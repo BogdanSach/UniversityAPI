6a73d1a4c3fcd2aa415d68445ca84bf66003eeec
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-12 19:19:59 +0300

    Add project files.

---
+++ University/Data/UniDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityAPI.Models.Domain;
+
+namespace UniversityAPI.Data
+{
+    public class UniDbContext : DbContext
+    {
+        public UniDbContext(DbContextOptions<UniDbContext> options) : base(options)
+        {   
+        }
+
+        public DbSet<University> Universities { get; set; }
+    }
+}
+
---
+++ University/Migrations/20251012160616_Initial Migration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Universities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Universities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Universities");
+        }
+    }
+}
