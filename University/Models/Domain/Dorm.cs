c1c6e9a320f7cdcd8087351707d59a50a3159489
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-12 19:35:56 +0300

    Created Dorm Domain Model

---
+++ University/Models/Domain/Dorm.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversityAPI.Models.Domain
+{
+    public class Dorm
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        
+
+
+        // Navigation properties
+        public Guid UniversityId { get; set; }
+        public Guid AddressId { get; set; }
+    }
+}
