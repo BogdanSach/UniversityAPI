6d2819621163a667fc1667eafabf0780b558090e
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-14 11:40:03 +0300

    Added University Dto

---
+++ University/Models/DTO/UniversityDto.cs
@@ -0,0 +1,15 @@
+﻿using UniversityAPI.Models.Domain;
+
+namespace UniversityAPI.Models.DTO
+{
+    public class UniversityDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Url { get; set; }
+
+        // Navigation properties
+        public ICollection<Dorm> Dorms { get; set; }
+    }
+}
