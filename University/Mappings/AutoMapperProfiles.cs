2e779ce62a6513314d8316de82c44003e9bb27f6
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-20 21:36:23 +0300

    Added file for AutoMapper profiles and added the first profile

---
+++ University/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UniversityAPI.Models.Domain;
+using UniversityAPI.Models.DTO;
+
+namespace UniversityAPI.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<University, UniversityDto>().ReverseMap();
+            
+        }
+    }
+}
