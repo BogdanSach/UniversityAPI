5ab7b10fc0ee394c20d890f4b17292d64e031956
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-20 19:14:30 +0300

    Created Sql repository for universities to change and use information from database

---
+++ University/Repositories/SqlUniversityRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityAPI.Data;
+using UniversityAPI.Models.Domain;
+
+namespace UniversityAPI.Repositories
+{
+    public class SqlUniversityRepository : IUniversityRepository
+    {
+        private readonly UniDbContext dbContext;
+
+        public SqlUniversityRepository(UniDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<University> CreateAsync(University university)
+        {
+            await dbContext.Universities.AddAsync(university);
+            await dbContext.SaveChangesAsync();
+            return university;
+        }
+
+        public async Task<University?> DeleteAsync(Guid id)
+        {
+            var existingUniversity = await dbContext.Universities.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingUniversity == null)
+            {
+                return null;
+            }
+
+            dbContext.Universities.Remove(existingUniversity);
+            await dbContext.SaveChangesAsync();
+            return existingUniversity;
+        }
+
+        public async Task<IEnumerable<University>> GetAllAsync()
+        {
+            return await dbContext.Universities.ToListAsync();
+        }
+
+        public async Task<University?> GetByIdAsync(Guid id)
+        {
+            return await dbContext.Universities.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<University?> UpdateAsync(Guid id, University university)
+        {
+            var existingUniversity = await dbContext.Universities.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingUniversity == null)
+            {
+                return null;
+            }
+            existingUniversity.Name = university.Name;
+            existingUniversity.Address = university.Address;
+            existingUniversity.Url = university.Url;
+
+            await dbContext.SaveChangesAsync();
+            return existingUniversity;
+        }
+    }
+}
