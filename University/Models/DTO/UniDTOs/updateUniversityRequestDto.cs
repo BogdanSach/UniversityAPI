5070cf9bbb1df1d4d81cdeacc358bdfaacea6111
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-20 21:37:01 +0300

    Added DTOs for handling adding and updating information

---
+++ University/Models/DTO/UniDTOs/updateUniversityRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityAPI.Models.DTO.UniDTOs
+{
+    public class updateUniversityRequestDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Url { get; set; }
+        public string? Description { get; set; }
+    }
+}
