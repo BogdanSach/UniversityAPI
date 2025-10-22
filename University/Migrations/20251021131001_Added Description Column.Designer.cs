e2385489330ad7d4bde0cf137aae2c4d52e2993d
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-22 22:36:15 +0300

    Migrations

--- University/Migrations/20251021131001_Added Description Column.Designer.cs
+++ University/Migrations/20251021131001_Added Description Column.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UniversityAPI.Data;
 
@@ -11,9 +12,11 @@ using UniversityAPI.Data;
 namespace UniversityAPI.Migrations
 {
     [DbContext(typeof(UniDbContext))]
-    partial class UniDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251021131001_Added Description Column")]
+    partial class AddedDescriptionColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
