35ebdf730416a08645ad3959cf628532b282b66b
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-30 18:11:14 +0200

    Added interface for methods for dorms

---
+++ University/Repositories/DormRepos/IDormRepository.cs
@@ -0,0 +1,13 @@
+﻿using UniversityAPI.Models.Domain;
+
+namespace UniversityAPI.Repositories.DormRepos
+{
+    public interface IDormRepository
+    {
+        Task<IEnumerable<Dorm>> GetAllAsync();
+        Task<Dorm?> GetByIdAsync( Guid id );
+        Task<Dorm> CreateAsync(Dorm dorm);
+        Task<Dorm?> UpdateAsync(Guid id, Dorm dorm);
+        Task<Dorm?> DeleteAsync(Guid id);
+    }
+}
