636f9e6224087017fc4c6a94fc6039701590f3e2
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-30 19:49:12 +0200

    Method for searching dorms for specific university

--- University/Repositories/DormRepos/IDormRepository.cs
+++ University/Repositories/DormRepos/IDormRepository.cs
@@ -5,7 +5,8 @@ namespace UniversityAPI.Repositories.DormRepos
     public interface IDormRepository
     {
         Task<IEnumerable<Dorm>> GetAllAsync();
-        Task<Dorm?> GetByIdAsync( Guid id );
+        Task<Dorm?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Dorm>> GetByUniversityIdAsync(Guid universityId);
         Task<Dorm> CreateAsync(Dorm dorm);
         Task<Dorm?> UpdateAsync(Guid id, Dorm dorm);
         Task<Dorm?> DeleteAsync(Guid id);
