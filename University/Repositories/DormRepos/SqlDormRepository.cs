d84c9087a6b53b3e4521080bce3f60102736816e
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-10-30 18:27:34 +0200

    Created repository for CRUD for changing dorms data in database

---
+++ University/Repositories/DormRepos/SqlDormRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityAPI.Data;
+using UniversityAPI.Models.Domain;
+
+namespace UniversityAPI.Repositories.DormRepos
+{
+    public class SqlDormRepository : IDormRepository
+    {
+        private readonly UniDbContext dbContext;
+
+        public SqlDormRepository(UniDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Dorm> CreateAsync(Dorm dorm)
+        {
+            await dbContext.Dorms.AddAsync(dorm);
+            await dbContext.SaveChangesAsync();
+
+            return dorm;
+        }
+
+        public async Task<Dorm?> DeleteAsync(Guid id)
+        {
+            var existingDorm = await dbContext.Dorms.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingDorm != null) { return null; }
+
+            dbContext.Dorms.Remove(existingDorm);
+            await dbContext.SaveChangesAsync();
+            return existingDorm;
+        }
+
+        public async Task<IEnumerable<Dorm>> GetAllAsync()
+        {
+            var dorms = await dbContext.Dorms
+                .Include(d => d.Location)
+                .ToListAsync();
+            return dorms;
+        }
+
+        public async Task<Dorm?> GetByIdAsync(Guid id)
+        {
+            var dorm = await dbContext.Dorms
+                .Include (d => d.Location)
+                .FirstOrDefaultAsync(x =>x.Id == id);
+
+            return dorm;
+        }
+
+        public async Task<Dorm?> UpdateAsync(Guid id, Dorm dorm)
+        {
+            var existingDorm = await dbContext.Dorms
+                .Include(d => d.Location)
+                .FirstOrDefaultAsync(x =>x.Id == id);
+
+            if (existingDorm == null) { return null; }
+
+            existingDorm.PriceOfLiving = dorm.PriceOfLiving;
+            existingDorm.Capacity = dorm.Capacity;
+            existingDorm.DormTypeId = dorm.DormTypeId;
+
+            if (existingDorm.Location != null && dorm.Location != null)
+            {
+                existingDorm.Location.City = dorm.Location.City;
+                existingDorm.Location.Region = dorm.Location.Region;
+                existingDorm.Location.Street = dorm.Location.Street;
+                existingDorm.Location.Number = dorm.Location.Number;
+            }
+
+            await dbContext.SaveChangesAsync();
+            return existingDorm;
+        }
+    }
+}
