60251592da640dfd678bc363a86ecc0442ce57fb
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-11-13 18:21:10 +0200

    Added and removed some fields

--- University/Models/DTO/DormDTOs/DormDto.cs
+++ University/Models/DTO/DormDTOs/DormDto.cs
@@ -1,13 +1,18 @@
-﻿namespace UniversityAPI.Models.DTO.DormDTOs
+﻿using UniversityAPI.Models.Domain;
+
+namespace UniversityAPI.Models.DTO.DormDTOs
 {
     public class DormDto
     {
         public Guid Id { get; set; } //PK
-        public string Type { get; set; }
+        public int Number { get; set; }
+        public decimal PriceOfLiving { get; set; }
         public int Capacity { get; set; }
 
         // Navigation properties
-        public LocationDto LocationDto { get; set; }
+        public LocationDto Location { get; set; }
         public Guid UniversityId { get; set; }
+        public string UniversityName { get; set; }
+        public DormTypeDto DormType { get; set; }
     }
 }
