634305d69af04ef45eecc5c3a75af49fcd851e3a
BogdanSach <sachkovskyi_ak23@nuwm.edu.ua>
2025-11-13 15:07:54 +0200

    Interface for unibuilding methods

---
+++ University/Repositories/UniBuildingRepos/IUniversityBuildingRepository.cs
@@ -0,0 +1,17 @@
+﻿using UniversityAPI.Models.Domain;
+
+
+namespace UniversityAPI.Repositories.UniBuildingRepos
+{
+    public interface IUniversityBuildingRepository
+    {
+        Task<IEnumerable<UniversityBuilding>> GetAllAsync();
+        Task<UniversityBuilding?> GetByIdAsync(Guid id);
+        Task<IEnumerable<UniversityBuilding>> GetByUniversityIdAsync(Guid universityId);
+        Task<UniversityBuilding?> GetByIdForUniAsync(Guid universityId, Guid id);
+        Task<UniversityBuilding> CreateAsync(UniversityBuilding universityBuilding);
+        Task<UniversityBuilding?> UpdateAsync(Guid universityId, Guid id, UniversityBuilding universityBuilding);
+        Task<UniversityBuilding?> DeleteAsync(Guid universityId, Guid id);
+
+    }
+}
